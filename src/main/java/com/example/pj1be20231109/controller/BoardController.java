1a0d413c44c28599b410b8f7096d7572e1306cff
JungMo <jmjm0611@naver.com>
2023-11-09 15:43:33 +0900

    게시물 보기

--- src/main/java/com/example/pj1be20231109/controller/BoardController.java
+++ src/main/java/com/example/pj1be20231109/controller/BoardController.java
@@ -35,4 +35,9 @@ public class BoardController {
         return service.list();
     }
 
+    @GetMapping("/id/{id}")
+    public Board get(@PathVariable Integer id){
+        return service.get(id);
+    }
+
 }
--- src/main/java/com/example/pj1be20231109/mapper/BoardMapper.java
+++ src/main/java/com/example/pj1be20231109/mapper/BoardMapper.java
@@ -11,15 +11,23 @@ import java.util.List;
 public interface BoardMapper {
 
     @Insert("""
-        INSERT INTO board (title, content, writer)
-        VALUES (#{title}, #{content}, #{writer})
-    """)
+                INSERT INTO board (title, content, writer)
+                VALUES (#{title}, #{content}, #{writer})
+            """)
     int insert(Board board);
 
     @Select("""
-        SELECT id, title, writer, inserted
-        FROM board
-        ORDER BY id DESC
-    """)
+                SELECT id, title, writer, inserted
+                FROM board
+                ORDER BY id DESC
+            """)
     List<Board> selectAll();
+
+
+    @Select("""
+                SELECT *
+                FROM board
+                WHERE id = #{id}
+            """)
+    Board selectById(Integer id);
 }
